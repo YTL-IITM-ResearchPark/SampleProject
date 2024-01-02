d7673fa2ff8f45e8e21e4383172917156c96492a
saisritejakuppa <saisritejakuppa@gmail.com>
2024-01-02 19:42:03 +0530

    init commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.add import add
+
+# add two numbers
+print(add(1, 2))
---
+++ src/add.py
@@ -0,0 +1,11 @@
+import math
+
+def sum(a, b):
+    """
+    Returns the sum of two numbers.
+    """
+    return a + b
+
+if __name__ == '__main__':
+    print(sum(1, 2))
+    print(math.pi)
